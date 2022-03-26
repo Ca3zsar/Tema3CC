5ad8d55402cfc49f4bf928a55a1434839dda52f3
Cezar Todirisca <cezartodirisca@gmail.com>
2022-03-26 21:41:16 +0200

    Refactored routes + call from front to back

---
+++ API/twitter/login.py
@@ -0,0 +1,20 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import Account
+from django.http import HttpResponse
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        if username == "" or password == "":
+            return HttpResponse("Fields username and password must exist!", status=400)
+
+        response_data = {}
+        response_data["message"] = f"{username} - {password}"
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    else:
+        return HttpResponse("Method not allowed", status=405)
--- API/twitter/urls.py
+++ API/twitter/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from . import views
+from . import register
+from . import login
+from . import tweet
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tweet-url', views.tweet, name='tweet-post'),
-    path('register', views.register, name='register')
+    path('tweet-url', tweet.tweet, name='tweet-post'),
+    path('register', register.register, name='register'),
+    path('login', login.login, name='login'),
 ]
